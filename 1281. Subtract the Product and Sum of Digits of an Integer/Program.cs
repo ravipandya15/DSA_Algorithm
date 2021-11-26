07fdf34e017126a2a768c8f1a274107f48154055
Ravi Pandya <Bacancy@DESKTOP-HDC70TE>
2021-11-27 00:39:17 +0530

    Bitwise operator -> changes

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1281._Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1281. Subtract the Product and Sum of Digits of an Integer");
+            Console.ReadLine();
+        }
+
+        public int subtractProductAndSum(int n)
+        {
+            int prod = 1;
+            int sum = 0;
+
+            while (n != 0)
+            {
+                int digit = n % 10;
+                prod *= digit;
+                sum += digit;
+
+                n /= 10;
+            }
+
+            return prod - sum;
+        }
+    }
+}
---
+++ 191. Number of 1 Bits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _191._Number_of_1_Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("191. Number of 1 Bits");
+            Console.ReadLine();
+        }
+
+        public static int HammingWeight(uint n)
+        {
+            int count = 0;
+
+            while (n != 0)
+            {
+                //bool temp = Convert.ToBoolean(n & 1);
+                count = count + Convert.ToInt32(n & 1);
+                n = n >> 1;
+            }
+            return count;
+        }
+    }
+}
