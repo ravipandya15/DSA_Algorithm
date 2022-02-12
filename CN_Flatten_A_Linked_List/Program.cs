1b897f844501b62da728870243cb609a807c7ac6
Ravi Pandya <Bacancy@DESKTOP-HDC70TE>
2022-02-13 00:30:48 +0530

    LinkedList - Love bubber

---
+++ CN_Flatten_A_Linked_List/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace CN_Flatten_A_Linked_List
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node child;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+            this.child = null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CN_Flatten_A_Linked_List");
+            //Node head = new Node(1);
+            //Node next2 = new Node(4);
+            //Node next3 = new Node(7);
+            //Node next4 = new Node(9);
+            //Node next5 = new Node(20);
+
+            //Node down1 = new Node(2);
+            //Node down2 = new Node(3);
+            //Node down3 = new Node(5);
+            //Node down4 = new Node(6);
+            //Node down5 = new Node(8);
+            //Node down6 = new Node(12);
+
+            //head.next = next2;
+            //next2.next = next3;
+            //next3.next = next4;
+            //next4.next = next5;
+
+            //head.child = down1;
+            //down1.child = down2;
+
+            //next2.child = down3;
+            //down3.child = down4;
+
+            //next3.child = down5;
+
+            //next4.child = down6;
+
+            Node head = new Node(3);
+            Node next2 = new Node(5);
+            Node next3 = new Node(22);
+            Node next4 = new Node(26);
+            Node next5 = new Node(39);
+
+            Node down1 = new Node(4);
+            Node down2 = new Node(6);
+            Node down3 = new Node(11);
+            Node down4 = new Node(14);
+            Node down5 = new Node(25);
+            Node down6 = new Node(28);
+
+            //head.next = next2;
+            //next2.next = next3;
+            //next3.next = next4;
+            //next4.next = next5;
+
+            head.child = down1;
+            down1.child = down2;
+
+            next2.child = down3;
+            down3.child = down4;
+
+            next3.child = down5;
+
+            next4.child = down6;
+
+            Node result = flattenLinkedList(head);
+        }
+
+        public static Node merge(Node down, Node right)
+        {
+            Node ans = new Node(-1);
+            Node temp = ans;
+
+            while (down != null && right != null)
+            {
+                if (down.data < right.data)
+                {
+                    temp.next = down;
+                    temp = temp.next;
+                    down = down.child;
+                    temp.child = null;
+                }
+                else
+                {
+                    temp.next = right;
+                    temp = temp.next;
+                    right = right.next;
+                }
+            }
+
+            while (down != null)
+            {
+                temp.next = down;
+                temp = temp.next;
+                down = down.child;
+                temp.child = null;
+            }
+
+            while (right != null)
+            {
+                temp.next = right;
+                temp = temp.next;
+                right = right.next;
+            }
+
+            return ans.next;
+        }
+
+        public static Node flattenLinkedList(Node start)
+        {
+            if (start == null) return start;
+
+            Node down = start;
+
+            Node right = flattenLinkedList(down.next);
+            down.next = null;
+
+            Node result = merge(down, right);
+
+            return result;
+        }
+    }
+}
---
+++ CN_MergeSort_Linked_List/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace CN_MergeSort_Linked_List
+{
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CN_MergeSort_Linked_List");
+        }
+
+        private static Node<int> findMid(Node<int> head)
+        {
+            Node<int> slow = head;
+            Node<int> fast = head.next;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+
+        private static Node<int> mergeSortedList(Node<int> left, Node<int> right)
+        {
+            if (left == null) return right;
+            if (right == null) return left;
+
+            Node<int> ans = new Node<int>(-1);
+            Node<int> temp = ans;
+
+            while (left != null && right != null)
+            {
+                if (left.data < right.data)
+                {
+                    temp.next = left;
+                    temp = left; // or temp = temp.next
+                    left = left.next;
+                }
+                else
+                {
+                    temp.next = right;
+                    temp = right;
+                    right = right.next;
+                }
+            }
+
+            if (left != null)
+            {
+                temp.next = left;
+            }
+            else if (right != null)
+            {
+                temp.next = right;
+            }
+
+            return ans.next;
+        }
+
+        public static Node<int> mergeSort(Node<int> head)
+        {
+
+            if (head == null || head.next == null) return head;
+
+            // find mid;
+            Node<int> mid = findMid(head);
+            Node<int> left = head;
+            Node<int> right = mid.next;
+            mid.next = null;
+
+            // recursively sort left and right part
+            left = mergeSort(left);
+            right = mergeSort(right);
+
+            // merge sorted List
+            Node<int> result = mergeSortedList(left, right);
+
+            return result;
+        }
+    }
+}
