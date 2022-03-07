9801ea5299de0eee97f917d10bbbfa0d44cfdda9
Ravi Pandya <Bacancy@DESKTOP-HDC70TE>
2022-03-08 00:10:19 +0530

    IMP String Problems

---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _392._Is_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("392. Is Subsequence");
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0) return true;
+            int i = 0;
+            for (int j = 0; j < t.Length; j++)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+
+                if (i == s.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 796. Rotate String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _796._Rotate_String
+{
+    class Program
+    {
+        // asked in Qualcomm
+        static void Main(string[] args)
+        {
+            Console.WriteLine("796. Rotate String");
+        }
+
+        public bool RotateString(string s, string goal)
+        {
+            return ((s.Length == goal.Length) && (s + s).IndexOf(goal) != -1);
+        }
+    }
+}
